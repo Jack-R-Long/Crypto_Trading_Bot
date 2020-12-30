5a03ff67e8de3493d5734bdfeb4aae4a88db6cb8
jack.long7 <jack.richard.long@gmail.com>
2020-12-30 13:08:19 -0600

    Logic for RSI overbought and oversold

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
-import websocket, json, pprint
+import websocket, json, pprint, talib, numpy
+import 
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30 
+TRADE_SYMBOL = "ETHUSD"
+TRADE_QUANITITY = 0.1 
 
 closes = []
+in_position = False  # Not bought
 
 def on_message(ws, message):
 	global closes
@@ -19,8 +26,26 @@ def on_message(ws, message):
 		closes.append(float(close))
 		print("Closes\n {}".format(closes))
 
+		if len(closes) > RSI_PERIOD:  # Need at least 14 closes to calculate RSI 14
+			np_closes = numpy.array(closes)
+			rsi = talib.RSI(np_closes, RSI_PERIOD)
+			last_rsi = rsi[-1]
+			print("RSI values so far \n{}".format(rsi))
+			print("Current RSI is: {}".format(last_rsi))
 
-
+			if last_rsi > RSI_OVERBOUGHT:
+				if in_position:
+					print("Sell sell sell!")
+					# put sell through binance
+				else:
+					print("It is overbought but we don't own any")
+			
+			if last_rsi < RSI_OVERSOLD:
+				if in_position:
+					print("Oversold but we are already in position")
+				else:
+					print("Buy buy buy!")
+					# Put Binance buy here
 
 def on_open(ws):
 	print("Socket Opened")
