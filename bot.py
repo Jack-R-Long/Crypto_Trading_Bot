f6046427c6351655f82f324767a83a08020410e9
jack.long7 <jack.richard.long@gmail.com>
2020-12-30 10:39:11 -0600

    Grabbing close data from socket stream

--- bot.py
+++ bot.py
@@ -2,18 +2,22 @@ import websocket, json, pprint
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+closes = []
 
 def on_message(ws, message):
+	global closes
 	print("Message recived")
 	json_message = json.loads(message)
 	# pprint.pprint(json_message)
 	
-	candle = message['k']
+	candle = json_message['k']
 	is_candle_closed = candle['x']
 	close = candle['c']
 
 	if is_candle_closed:
 		print('Candle is closed at {}'.format(close))
+		closes.append(float(close))
+		print("Closes\n {}".format(closes))
 
 
 
